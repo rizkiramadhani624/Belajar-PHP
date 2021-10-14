4017236b34fe93ad1bd7f1655a7d83217448e60b
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-10-14 16:49:31 +0700

    Add files via upload
    Begitu web server menemukan tag <?php pada baris ke-7, maka tag tersebut menginstruksikan kepada web server bahwa kode selanjutnya terdiri dari bahasa PHP, sehingga apa pun yang kita tulis setelah tag ini akan diproses mengikuti aturan bahasa pemrograman PHP. Kita sebut saja proses ini sebagai PHP mode.
    Mulai dari tag <?php adalah PHP mode. Dalam PHP mode inilah kita menulis kode pemrograman PHP. Untuk contoh di atas, saya membuat sebuah perintah sederhana PHP, yakni echo. Echo adalah perintah PHP untuk menampilkan text yang terdapat di antara kedua tanda kutip (“). Dalam contoh di atas echo dipakai untuk menampilkan teks: “<p>Kalimat ini dibuat menggunakan PHP</p>” ke dalam web browser.
    Web server akan terus berada di PHP mode sampai menemukan tag penutup PHP, yakni tag ?> yang menandakan akhir dari PHP.
    Tag ?> selain tanda keluar dari mode PHP, juga menginstruksikan kepada web server bahwa text berikutnya akan diproses sebagai text HTML biasa.

---
+++ cara_kerja1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Belajar PHP</title>
+</head>
+<body>
+   <p>Kalimat ini dibuat menggunakan HTML saja </p>
+   <?php
+      echo "<p>Kalimat ini dibuat menggunakan PHP </p>";
+   ?>
+</body>
+</html>
