fda467c73ffda96a85ab80942098035ef6630da9
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-10-14 17:39:47 +0700

    Add files via upload
    penulisan komentar diawali semicolon (;) pd awal baris, setelang looping tdl ada semiclolon dibelakang kurung kurawal, spasi dan tab tdk dianggap oleh php.

---
+++ aturan1.php
@@ -0,0 +1,15 @@
+   echo "Hello, world";
+   sebuah_fungsi(21, "duniailkom");
+   $a = 1;
+   $nama = "duniailkom";
+   $b = $a / 25.0;
+   if ($y == $z) {
+     echo "Tampilkan Tabel";
+   }
+   echo "Ini kalimat pertama"; echo "Ini kalimat kedua"; $nama="dikoding";
+   ; ditas mode ribet
+   echo "Ini kalimat pertama";
+   echo "Ini kalimat kedua";
+   $nama = "dikoding";
+   ;diatas mode rapi
+?>
