28402dcdb2b48a2dca52d41fea6ba188f9bf7ae7
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-10-16 15:08:27 +0700

    Add files via upload

---
+++ logika if else.php
@@ -0,0 +1,27 @@
+<?php
+$nama1="Budi";//opsi 1
+ 
+if ($nama1=="Andi")
+   echo "Selamat Datang Andi...";
+else
+   echo "Selamat Datang di Dikoding";
+#####################################
+$nama=2"Bobo";//opsi 2
+ 
+if ($nama2=="Anda")
+{
+   echo "Selamat Datang Anda...";
+   echo "Anda Memiliki 3 pesan di inbox...";
+}
+else
+{
+   echo "Maaf, anda tidak memiliki hak akses";
+}
+##############################################
+if (expression) ://opsi alternatif
+   statement1;
+   statement2;
+else:
+   statement3;
+endif
+?>
