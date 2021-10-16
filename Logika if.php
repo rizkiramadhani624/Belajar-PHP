e47f38327d4599ee48ca81f9e8c6c6a1f6e49eb8
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-10-16 14:58:54 +0700

    Add files via upload

---
+++ Logika if.php
@@ -0,0 +1,24 @@
+<?php
+if (expression1)//opsi biasa
+{
+   statement1;
+   statement2;
+}
+if (expression2)
+{
+   statement3;
+   statement4;
+}
+if (expression3) opsi bercabang
+{
+   statement1;
+   if (expression3)
+   {
+      statement1;
+   }
+}
+if (expression4) ://opsi alternatif
+   statement1;
+   statement1;
+endif
+?>
