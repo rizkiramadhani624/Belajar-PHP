147ceb6edba18dff1fa6d1326398ae89d0160f10
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-10-16 23:46:19 +0700

    Add files via upload

---
+++ memanggil_fungsi.php
@@ -0,0 +1,7 @@
+<?php
+$akar_kuadrat = sqrt(49);
+echo "Akar kuadrat dari 49 adalah $akar_kuadrat";
+// Akar kuadrat dari 49 adalah 7
+echo "12 pangkat 2 adalah: ".pow(12,2);
+// 12 pangkat 2 adalah: 144
+?>
